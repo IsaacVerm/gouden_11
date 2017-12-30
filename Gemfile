d36d2b802fa72acdbbc615e2428ee6760a75aad8
Isaac Verminck <isaacverm@gmail.com>
2017-12-30 21:06:31 +0100

    Add info by player to Scouts

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "https://rubygems.org"
 gem 'capybara'
 gem 'selenium-webdriver'
 gem 'rest-client'
+gem 'nokogiri'
+gem 'awesome_print'
--- lib/get_scouting.rb
+++ lib/get_scouting.rb
@@ -8,8 +8,7 @@ require 'rest-client'
 require_relative '../payload_builder/scouts_payload_builder'
 require_relative '../requests/scouts'
 
-payload_builder = ScoutsPayloadBuilder.new('412', '2')
-payload_builder.create_payload
-payload = payload_builder.payload
-
-puts Scouts.post(payload)
+scouts = Scouts.new(ScoutsPayloadBuilder.payload('412','2'))
+scouts.post
+scouts.response_to_html
+scouts.info_by_player
